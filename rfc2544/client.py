90a5cad39d7efb0be3a1cbedd9b4c6eaff084f03
Daniel Kostecki <dkosteck@redhat.com>
2023-06-13 10:51:49 -0400

    Attempting automated client run

--- rfc2544/client.py
+++ rfc2544/client.py
@@ -2,8 +2,11 @@ from __future__ import print_function
 import logging
 import argparse
 import requests
+import json
+import datetime
 
-def actionGetResult(args):
+
+def actionGetResult(args, returnResults=False):
     response = requests.get('http://' + args.server_addr + ":" + str(args.server_port) + "/result")
     if not response.json():
         print("test result not avalable.")
@@ -13,7 +16,7 @@ def actionGetResult(args):
         print("port %s rx_pps: %.2f" %(port, response.json()[port]["rx_pps"]))
         print("port %s rx_latency_average: %.2f" %(port, response.json()[port]["rx_latency_average"]))
 
-def actionStartTrafficgen(args):
+def actionStartTrafficgen(args, returnResults=False):
     json_data = {
         'l3': l3,
         'device_pairs': args.device_pairs,
@@ -33,19 +36,57 @@ def actionStartTrafficgen(args):
     response = requests.post('http://' + args.server_addr + ":" + str(args.server_port) + "/trafficgen/start", json=json_data)
     print("start trafficgen: %s" % ("success" if response.json() else "fail"))
 
-def actionStopTrafficgen(args):
+def actionStopTrafficgen(args, returnResults=False):
     response = requests.get('http://' + args.server_addr + ":" + str(args.server_port) + "/trafficgen/stop")
     print("stop trafficgen: %s" % ("success" if response.json() else "fail"))
 
-def actionStatus(args):
+def actionStatus(args, returnResults=False):
+    results = {}
     response = requests.get('http://' + args.server_addr + ":" + str(args.server_port) + "/trafficgen/running")
     print("trafficgen is currently%s running" %("" if response.json() else " not"))
+    results["running"] = response.json()
+    
     response = requests.get('http://' + args.server_addr + ":" + str(args.server_port) + "/result/available")
     print("test result is avalable: %s" % ("yes" if response.json() else "no"))
+    results["available"] = response.json()
 
-def actionGetMac(args):
+    if returnResults:
+        return results
+
+def actionGetMac(args, returnResults=False):
     response = requests.get('http://' + args.server_addr + ":" + str(args.server_port) + "/maclist")
     print("This trafficgen mac list: %s" %(response.json()))
+
+def actionAuto(args):
+    f = open(args.config)
+    json_data = json.load(f)
+
+    args.server_addr = json_data["server_addr"]
+    args.server_port = json_data["server_port"]
+    
+    if actionStatus(args, returnResults=True)["running"]:
+        assert actionStopTrafficgen(args, returnResults=True)
+
+    overwriteArgs(args, json_data)
+    assert actionStartTrafficgen(args, returnResults=True)
+
+    # Loop until timeout, checking for results
+    timeout_time = datetime.datetime.now() + datetime.timedelta(0, 60 * json_data["timeout"])
+    while True:
+        if datetime.datetime.now() > timeout_time:
+            print("Timed out waiting for results")
+            exit()
+        
+        if actionStatus(args, returnResults=True)["results"] and not actionStatus(args, returnResults=True)["running"]:
+            break
+    
+    results = actionGetResult(args, returnResults=True)
+    print(results)
+
+def overwriteArgs(args, json_data):
+    for field in json_data:
+        args.field = json_data[field]
+
 
 def run(args):
     if args.action == "start":
@@ -58,6 +99,8 @@ def run(args):
         actionGetResult(args)
     elif args.action == "get-mac":
         actionGetMac(args)
+    elif args.action == "auto":
+        actionAuto(args)
     else:
         print("invalid action: %s" %(args.action))
 
@@ -69,7 +112,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Trafficgen client')
     parser.add_argument('action',
                         help='specify what action the server will take',
-                        choices=['start', 'stop', 'status', 'get-result', 'get-mac']
+                        choices=['start', 'stop', 'status', 'get-result', 'get-mac', 'auto']
                         )
     parser.add_argument('--frame-size',
                         dest='frame_size',
@@ -153,5 +196,9 @@ if __name__ == '__main__':
                         type = str,
                         help="rate tolerance failure"
                         )
+    parser.add_argument("--config",
+                        dest="config",
+                        type=str,
+                        help="path to the config yaml")
     args = parser.parse_args()
     run(args)
